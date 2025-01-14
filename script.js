8ac377e4ddad6cf9d921a207dad8ac600cf62c5d
WHYGIKO <sardor.level@gmail.com>
2025-01-14 23:59:15 +0500

    lesson1

---
+++ script.js
@@ -0,0 +1,17 @@
+var name = prompt('Ismingizni kiriting: ')
+var age = +prompt('Yoshingizni kiriting: ')
+var plus = +prompt('Amalni bajaring: 6 + 3 = ?')
+var minus = +prompt('Amalni bajaring: 9 - 7 = ?' )
+var multi = +prompt('Amalni bajaring: 5 * 4 = ?' )
+var division = +prompt('Amalni bajaring: 3 / 1 = ?' )
+var percent = +prompt('Amalni bajaring: 50 % 2 = ?')
+alert('Javoblarni cansole dan tekshiring!')
+
+
+console.log(`Sizning ismingiz: ${name}`);
+console.log(`Sizning yoshingiz: ${age}`);
+console.log(`Misol 1: 6 + 3 = ${(6+3)} Sizning javobingiz: ${plus} `);
+console.log(`Misol 2: 9 - 7 = ${(9-7)} Sizning javobingiz: ${minus}`);
+console.log(`Misol 3: 5 * 4 = ${(5*4)} Sizning javobingiz: ${multi}`);
+console.log(`Misol 4: 3 / 1 = ${(3/1)} Sizning javobingiz: ${division}`);
+console.log(`Misol 5: 50 % 2 = ${(50 % 2)} Sizning javobingiz: ${percent} `);

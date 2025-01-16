b197f96d253a95523e2ca6e6e0cbf13cceda48c3
WHYGIKO <sardor.level@gmail.com>
2025-01-16 17:42:45 +0500

    JS lesson 2

--- script.js
+++ script.js
@@ -1,17 +1,70 @@
 var name = prompt('Ismingizni kiriting: ')
 var age = +prompt('Yoshingizni kiriting: ')
-var plus = +prompt('Amalni bajaring: 6 + 3 = ?')
-var minus = +prompt('Amalni bajaring: 9 - 7 = ?' )
-var multi = +prompt('Amalni bajaring: 5 * 4 = ?' )
-var division = +prompt('Amalni bajaring: 3 / 1 = ?' )
-var percent = +prompt('Amalni bajaring: 50 % 2 = ?')
+var a = 6
+var b = 3
+var c = 5
+var d = 8
+var e = 2
+var f = 20
+var correct1 = a + b;
+var correct2 = c - e;
+var correct3 = a * d;
+var correct4 = d / e;
+var correct5 = f % e;
+var answer1 = +prompt(`Amalni bajaring: ${a} + ${b} = ?`)
+var answer2 = +prompt(`Amalni bajaring: ${c} - ${e} = ?`)
+var answer3 = +prompt(`Amalni bajaring: ${a} * ${d} = ?` )
+var answer4 = +prompt(`Amalni bajaring: ${d} / ${e} = ?` )
+var answer5 = +prompt(`Amalni bajaring: ${f} % ${e} = ?`)
+if (confirm(`Siz kiritgan ma'lumotlarni tasdiqlaysizmi? Ismingiz: ${name} Yoshingiz: ${age}`))
+{ 
+    alert("Ma'lumotlar tasdiqlandi!");
+}
+else {
+    alert("Iltimos, ma'lumotlarni qayta kiriting.");
+    nameAgain = prompt(`Ismingizni kiriting: `)
+    ageAgain = +prompt(`Yoshingizni kiriting: `)
+    confirm(`Siz qayta kiritgan ma'lumotlar: Ismingiz: ${nameAgain} Yoshingiz: ${ageAgain}. Ma'lumotlarni tasdiqlaysizmi?`)
+}
+
 alert('Javoblarni cansole dan tekshiring!')
 
 
 console.log(`Sizning ismingiz: ${name}`);
 console.log(`Sizning yoshingiz: ${age}`);
-console.log(`Misol 1: 6 + 3 = ${(6+3)} Sizning javobingiz: ${plus} `);
-console.log(`Misol 2: 9 - 7 = ${(9-7)} Sizning javobingiz: ${minus}`);
-console.log(`Misol 3: 5 * 4 = ${(5*4)} Sizning javobingiz: ${multi}`);
-console.log(`Misol 4: 3 / 1 = ${(3/1)} Sizning javobingiz: ${division}`);
-console.log(`Misol 5: 50 % 2 = ${(50 % 2)} Sizning javobingiz: ${percent} `);
+
+if (answer1===correct1) {
+    console.log(`Misol1: ${a} + ${b} = ${correct1} Sizning javobigiz: ${answer1} Javob - to'g'ri`)
+    
+}
+else {
+    console.log(`Misol1: ${a} + ${b} = ${correct1} Sizning javobingiz: ${answer1} Javob - noto'g'ri `);
+}
+if (answer2===correct2) {
+    console.log(`Misol2: ${c} - ${e} = ${correct2} Sizning javobigiz: ${answer2} Javob - to'g'ri`)
+    
+}
+else {
+    console.log(`Misol2: ${c} - ${e} = ${correct2} Sizning javobingiz: ${answer2} Javob - noto'g'ri `);
+}
+if (answer3===correct3) {
+    console.log(`Misol3: ${a} * ${d} = ${correct1} Sizning javobigiz: ${answer3} Javob - to'g'ri`)
+    
+}
+else {
+    console.log(`Misol3: ${a} * ${d} = ${correct1} Sizning javobingiz: ${answer3} Javob - noto'g'ri `);
+}
+if (answer4===correct4) {
+    console.log(`Misol4: ${d} / ${e}} = ${correct4} Sizning javobigiz: ${answer4} Javob - to'g'ri`)
+    
+}
+else {
+    console.log(`Misol4: ${d} / ${e} = ${correct4} Sizning javobingiz: ${answer4} Javob - noto'g'ri `);
+}
+if (answer5===correct5) {
+    console.log(`Misol5: ${f} % ${e} = ${correct5} Sizning javobigiz: ${answer5} Javob - to'g'ri`)
+    
+}
+else {
+    console.log(`Misol5: ${f} % ${e} = ${correct5} Sizning javobingiz: ${answer5} Javob - noto'g'ri `);
+}
